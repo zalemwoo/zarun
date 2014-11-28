541dc21bf47f947c5447ee4f0b0844187e40e7a8
zalem <zalemwoo@gmail.com>
2014-11-28 21:51:51 +0800

    improve lua_script.js

--- test/js/lua/lua_script.js
+++ test/js/lua/lua_script.js
@@ -1,3 +1,11 @@
+(function(global){
+if(global.console === undefined){
+    var console = process.binding("console");
+}
+if(global.Lua === undefined){
+    console.error("Usage: " + process.argv[0] + " lua_patch.js lua.vm.js lua_script.js");
+    return;
+}
 var lua = new Lua.State();
-
 lua.execute("js.global.console:log('normal log')");
+})(this)
