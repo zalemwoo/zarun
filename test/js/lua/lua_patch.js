a339de681808b055f06951bd1bba437028b3dd06
zalem <zalemwoo@gmail.com>
2014-11-28 21:41:50 +0800

    add lua.vm.js

---
+++ test/js/lua/lua_patch.js
@@ -0,0 +1,3 @@
+var console = process.binding("console");
+this.print = console.log;
+var fileData0;
---
+++ test/js/lua/lua_script.js
@@ -0,0 +1,3 @@
+var lua = new Lua.State();
+
+lua.execute("js.global.console:log('normal log')");
